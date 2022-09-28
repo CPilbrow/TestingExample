cbf04ed19d408b43a7da5017a52e71c2ccd4d5e9
Colin Pilbrow <cpilbrow@waikato.ac.nz>
2022-09-28 16:24:01 +1300

    Added tests

--- HeapTests/UnitTest1.cs
+++ HeapTests/UnitTest1.cs
@@ -7,10 +7,57 @@ namespace HeapTests
     [TestClass]
     public class UnitTest1
     {
+        Heap heap;
+        [TestInitialize]
+        public void Initialize() {
+        heap = new Heap();
+        }
+
         [TestMethod]
         public void TestExample()
         {
             Assert.AreEqual(1 + 1, 2, "Test Description");
         }
+
+
+        [TestMethod]
+        public void InsertThenRemove()
+        {
+            heap.Insert("Hello");
+            string result = heap.Remove();
+            Assert.AreEqual("Hello", result);
+        }
+
+        [TestMethod]
+        public void RemoveFromEmptyHeap()
+        {
+            string result = heap.Remove();
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ABC()
+        {
+            heap.Insert("A");
+            heap.Insert("B");
+            heap.Insert("C");
+            Assert.AreEqual("A", heap.Remove());
+            Assert.AreEqual("B", heap.Remove());
+            Assert.AreEqual("C", heap.Remove());
+        }
+
+        [TestMethod]
+        public void CBA()
+        {
+            heap.Insert("C");
+            heap.Insert("B");
+            heap.Insert("A");
+            Assert.AreEqual("A", heap.Remove());
+            Assert.AreEqual("B", heap.Remove());
+            Assert.AreEqual("C", heap.Remove());
+        }
+
     }
+
+
 }
