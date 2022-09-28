5a1684ed47e7fedf40256ca94d3e8938e2647eb4
Colin Pilbrow <cpilbrow@waikato.ac.nz>
2022-09-28 18:06:43 +1300

    Implement better Remove method

--- TestingExample/Heap.cs
+++ TestingExample/Heap.cs
@@ -17,7 +17,7 @@ namespace TestingExample
             // 1. Add a node for the new element into the left - most empty slot in the lowest level of the tree
             heap.Add(s);
 
-            // 2. If the new node is the root of the tree, or the value of the new node is smaller than or equal to its parent, stop the algorithm
+            // 2. If the new node is the root of the tree, or the value of the new node is larger than or equal to its parent, stop the algorithm
             while (!(newPos == 0 || String.Compare(s,heap[(newPos-1)/2]) >= 0))
             {
                 // 3. Swap the node with its parent and go back to Step 2
@@ -33,7 +33,40 @@ namespace TestingExample
         public string Remove()
         {
             string topOfHeap = heap[0];
-            heap.RemoveAt(0);
+
+            // 1. Replace the root node by the right-most node at the bottom level of the tree
+            string migratedNode = heap[heap.Count - 1];
+            heap[0] = migratedNode;
+            int newPos = 0;
+            while (true)
+            {
+                int leftChildPos = newPos * 2 + 1;
+                int rightChildPos = newPos * 2 + 2;
+
+                // 2. If the migrated node is smaller or equal to all of its successor nodes, stop the algorithm
+                int greatestSuccessorPos = 0;
+                // if there are no successor nodes, stop
+                if (leftChildPos >= heap.Count) break;
+                // if there is only a left successor, then if it is larger, stop
+                if (rightChildPos >= heap.Count)
+                {
+                    if (String.Compare(heap[leftChildPos], migratedNode) >= 0) break;
+                    greatestSuccessorPos = leftChildPos;
+                }
+                else
+                {
+                    // If there are two successors, check which is smaller first.
+                    greatestSuccessorPos = String.Compare(heap[leftChildPos], heap[rightChildPos]) < 0 ? leftChildPos : rightChildPos;
+                    if (String.Compare(heap[greatestSuccessorPos], migratedNode) >= 0) break;
+                }
+                // 3. Swap the migrated node with its smallest successor and go back to Step 2
+                string greatestSuccessor = heap[greatestSuccessorPos];
+                heap[newPos] = greatestSuccessor;
+                heap[greatestSuccessorPos] = migratedNode;
+                newPos = greatestSuccessorPos;
+            }
+
+            heap.RemoveAt(heap.Count - 1);
             return topOfHeap;
         }
     }
